4bcce15bf198ef921cbd09eec2143078b2e568bd
Anthony Medina <antmedina91@gmail.com>
2019-01-21 23:21:10 +0000

    extract logic from main

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,12 @@ fn main() {
     println!("Searching for \"{}\"", config.query);
     println!("In file {}", config.filename);
 
+    run(config);
+}
+
+fn run(config: Config) {
     let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
+        .expect("something went wrong reading the file");
 
     println!("With text:\n{}", contents);
 }
@@ -25,7 +29,7 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str>  {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
