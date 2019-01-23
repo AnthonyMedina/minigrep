b69d9daba689a4d0b7e2e58195e83193dd0e7c4a
Anthony Medina <antmedina91@gmail.com>
2019-01-23 22:54:08 +0000

    remove printlns

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,6 @@ use std::error::Error;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
     Ok(())
 }
 
