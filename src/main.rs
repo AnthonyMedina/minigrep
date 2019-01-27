67b28aee7b44ad6c50da90de1da9898d78bf7175
Anthony Medina <antmedina91@gmail.com>
2019-01-27 23:40:06 +0000

    add missing eprintln

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
@@ -18,4 +18,3 @@ fn main() {
         process::exit(1);
     }
 }
-
